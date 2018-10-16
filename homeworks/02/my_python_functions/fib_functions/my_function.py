708a086c9418a4d226883370572250ba0b339279
Kron610 <ger.korekov@mail.ru>
2018-10-16 17:39:41 +0300

    homework_02

---
+++ homeworks/02/my_python_functions/fib_functions/my_function.py
@@ -0,0 +1,19 @@
+def cache_decorator(function):
+    mem_cache = {}
+    
+    def cached_fucntion(argument):
+        if argument in mem_cache:
+            return mem_cache[argument]
+        else:
+            mem_cache[argument] = function(argument)
+            return mem_cache[argument]
+    
+    return cached_fucntion
+@cache_decorator
+def fib(n):
+     if n==0:
+        return 1
+     if n==1:
+        return 1
+     return fib(n-1)+fib(n-2)
+from my_python_functions.fib_functions.my_function import fib
