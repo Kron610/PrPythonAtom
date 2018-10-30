4ef6677c10bc31c32aadff50397869b5c2480d72
Kron610 <ger.korekov@mail.ru>
2018-10-31 00:22:39 +0300

    Added KNN and flask_server

--- homeworks/03/flask_json_server.py
+++ homeworks/03/flask_json_server.py
@@ -1,17 +1,27 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, json
 
 app = Flask(__name__)
 
 @app.route("/get_classifier_result/<version>", methods=['GET', 'POST'])
 def return_classifier_result(version):
-    #TODO прочитать из полученного запроса json-контент
-    #В случае, если version==1, то должен вернуться json с версией и полем predict из входящего jsonа {"version":1, "predict":<predict_value>}
-    #В случае, если version==0, то должен вернуться json с версией и полем old_predict из входящего jsonа {"version":0, "predict":<old_predict_value>}
+    if request.method == 'POST':
+        if version == '1':
+            return json.dumps({'version' : 1, 'predict': request.get_json()['predict']})
+        else:
+            return json.dumps({'version' : 0, 'predict' :request.get_json()['old_predict']})
 
 @app.route("/")
 def hello():
-    #TODO должна возвращатьс инструкция по работе с сервером
-    return
+    return """Для работы с сервером нужно отправить запрос с помощью requests.post(url, data, headers)
+    URL имеет вид url/get_classifier_result/<version>, где <version> - '1' или '0'
+    1 - для новой версии, 0 - для старой
+    Треьуется отправить файл в формате json. Файл представляет из себя dict 
+    {"predict" : i} - при <version> - '1', 
+    {"old_predict": i} при <version> - '0'
+    Ответом на запрос является dict:
+    {"version" : value, "predict" : value}
+    На ['version'] возвращается 1 или 0, на ['predict'] отправленное(ые значение)
+    """
 
 if __name__ == "__main__":
     app.run()
